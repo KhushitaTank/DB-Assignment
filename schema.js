934b894aa1f92e893a540e1410a58d6cc6134073
KhushitaTank <120798835+KhushitaTank@users.noreply.github.com>
2024-03-11 14:19:59 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,1 +1,31 @@
+model ProductCategory {
+  id          Int      @id @default(autoincrement())
+  name        VarChar(255) @unique
+  desc        Text?
+  createdAt   DateTime  @default(now())
+  modifiedAt  DateTime  @updatedAt
+}
+
+model Product {
+  id          Int      @id @default(autoincrement())
+  name        VarChar(255)
+  desc        Text?
+  sku         VarChar(255)  @unique
+  categoryId  Int      @db.ForeignKey("ProductCategory")
+  inventory   Inventory?
+  price       Decimal 
+  discountId  Int?
+  createdAt   DateTime  @default(now())
+  modifiedAt  DateTime  @updatedAt
+  deletedAt   DateTime?
+  category    ProductCategory  @relation(fields: [categoryId], references: [id])
+}
+
+model Inventory {
+  id          Int      @id @default(autoincrement())
+  quantity    Int
+  createdAt   DateTime  @default(now())
+  modifiedAt  DateTime  @updatedAt
+  deletedAt   DateTime?
+}
 
